7b4182f482e685cc076c0ea87cba08e297a34b4a
Danieldesmond1 <danieldesmon953@gmail.com>
2025-03-31 18:11:02 -0700

    Removed unnecessary frontend and backend folders inside frontend

--- frontend/backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { initializePayment, verifyPayment } from "../controllers/paymentController.js";
-
-const router = express.Router();
-
-router.post("/paystack/initiate", initializePayment);
-router.get("/paystack/verify", verifyPayment);
-
-export default router;
